f38f83dc905c3290cbfa72b328dfb351f7dd59fc
Bell Eapen <github@gulfdoctor.net>
2018-10-24 13:49:39 -0400

    HelloPatient added.

--- src/services/conditionService.js
+++ src/services/conditionService.js
@@ -1,14 +1,12 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-Vue.use(Vuex);
+import FHIR from 'fhirclient';
 
-let Condition = function(patient_id){
+let Conditions = function(patient_id){
   var smart = FHIR.client({
     serviceUrl: 'https://r2.smarthealthit.org',
     patientId: 'smart-1137192'
   });
-  var conditions = smart.patient.api.search({type: 'Condition'});
-  return new Vuex.Store(conditions);
+  // $.Deferred
+  return smart.patient.api.search({type: 'Condition'});
 };
 
-export default Condition;
+export default Conditions;
