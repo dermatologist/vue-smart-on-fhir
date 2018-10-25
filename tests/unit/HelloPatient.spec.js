7f4abd31be156dfc30d99cead0463a6525e75c07
Bell Eapen <github@gulfdoctor.net>
2018-10-25 15:03:51 -0400

    Tests added

--- tests/unit/HelloPatient.spec.js
+++ tests/unit/HelloPatient.spec.js
@@ -1,11 +1,11 @@
 import { expect } from 'chai';
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import HelloPatient from '@/components/HelloPatient.vue';
 
-describe('HelloWorld.vue', () => {
+describe('HelloPatient.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(HelloPatient, {
       propsData: { msg },
     });
     expect(wrapper.text()).to.include(msg);
